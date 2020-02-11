0c05430a8690f3d40f1fd22b64159167bae932e8
berryllium <gorkundp@yandex.ru>
2020-02-12 01:44:44 +0300

    redirect code 308

--- app/Http/Controllers/GlossaryController.php
+++ app/Http/Controllers/GlossaryController.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 
 class GlossaryController extends Controller
 {
-    // получаем из базы данных полный URL по скоращенному
+    // получаем из базы данных полный URL по скоращенному и делаем редирект с кодом 308
     public function index($url_short) {
         $url_full =  Glossary::where('url_short', $url_short)->select('url_full')->first()['url_full'];
-        return redirect($url_full);
+        return redirect($url_full, 308);
     }
 }

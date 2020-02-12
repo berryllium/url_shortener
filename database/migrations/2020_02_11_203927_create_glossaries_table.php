99eff4c4ea6baf43177e7ef11f5e56d7fe34149a
berryllium <gorkundp@yandex.ru>
2020-02-12 15:46:52 +0300

    обновление полей

--- database/migrations/2020_02_11_203927_create_glossaries_table.php
+++ database/migrations/2020_02_11_203927_create_glossaries_table.php
@@ -17,7 +17,8 @@ class CreateGlossariesTable extends Migration
             $table->bigIncrements('id');
             $table->string('url_full');
             $table->string('url_short')->index();
-            $table->timestamps();
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
